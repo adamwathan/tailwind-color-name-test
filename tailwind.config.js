6cf18fc0ad7676f4a937c9b3c1f24e5a56dad646
Adam Wathan <adam.wathan@gmail.com>
2019-06-27 10:25:12 -0400

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,66 @@
+module.exports = {
+  theme: {
+    colors: {
+      transparent: 'transparent',
+
+      black: '#333333',
+      white: '#ffffff',
+
+      blue: {
+        lightest: 'rgba(0, 62, 105, 0.1)',
+        lighter: 'rgba(0, 62, 105, 0.15)',
+        light: '#075488',
+        default: '#003e69',
+        dark: '#003052',
+      },
+      teal: {
+        lightest: 'rgba(55, 145, 154, 0.1)',
+        lighter: 'rgba(55, 145, 154, 0.15)',
+        light: '#44b3bd',
+        default: '#37919a',
+        dark: '#2d8289',
+      },
+      orange: {
+        lightest: 'rgba(244, 123, 32, 0.1)',
+        lighter: 'rgba(244, 123, 32, 0.15)',
+        light: '#f29844',
+        default: '#f47b20',
+        dark: '#e86e12',
+      },
+      green: {
+        lightest: 'rgba(94, 151, 50, 0.1)',
+        lighter: 'rgba(94, 151, 50, 0.15)',
+        light: '#68a82c',
+        default: '#5e9732',
+        dark: '#4c8222',
+      },
+      red: {
+        lightest: 'rgba(237, 49, 38, 0.1)',
+        lighter: 'rgba(237, 49, 38, 0.15)',
+        light: '#ff453a',
+        default: '#ed3126',
+        dark: '#d5150b',
+      },
+      yellow: {
+        lightest: 'rgba(255, 183, 0, 0.1)',
+        lighter: 'rgba(255, 183, 0, 0.15)',
+        light: '#ffb700',
+        default: '#ffb700',
+        dark: '#ffb700',
+      },
+      gray: {
+        100: '#f7fafc',
+        200: '#edf2f7',
+        300: '#e2e8f0',
+        400: '#cbd5e0',
+        500: '#a0aec0',
+        600: '#718096',
+        700: '#4a5568',
+        800: '#2d3748',
+        900: '#1a202c',
+      },
+    }
+  },
+  variants: {},
+  plugins: [],
+}
